de4f3508d423132d26c74febfadc592a5d64235c
ccampbell1 <ccampbell1@E-D1H09H2-CC>
2020-04-04 23:25:30 -0400

    updates

--- App/start.js
+++ App/start.js
@@ -1,25 +1,11 @@
 const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
+
 const writeStream = fs.createWriteStream('post.csv');
 const csv = require('csv-parser');
-const filePath = './post.csv';
+const filePath = './csv.csv';
 const results = [];
 writeStream.write(`Title \n`);
 
-function scrape() {
-  request('https://www.guitarworld.com/blogs', (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-      const $ = cheerio.load(html);
-
-      $('h3').each((i, el) => {
-        const article = $(el).text();
-        writeStream.write(`${article}`);
-      });
-    }
-    toJSON();
-  });
-}
 
 function toJSON() {
   fs.createReadStream(filePath)
@@ -35,4 +21,4 @@ function toJSON() {
     });
 }
 
-scrape();
+toJSON()
