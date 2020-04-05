4dc576d1a9084ab348e07f31fe2c105a5126057f
ccampbell1 <ccampbell1@E-D1H09H2-CC>
2020-04-05 00:38:58 -0400

    updates

--- App/start.js
+++ App/start.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
-
-const writeStream = fs.createWriteStream('post.csv');
+const axios = require("axios");
 const csv = require('csv-parser');
-const filePath = './csv.csv';
+const filePath = "../CSV_Goes_Here/test.csv";
 const results = [];
-writeStream.write(`Title \n`);
 
 
-function toJSON() {
+function parseCSV() {
   fs.createReadStream(filePath)
     .on('error', () => {
       console.log('there is an error');
@@ -21,4 +19,23 @@ function toJSON() {
     });
 }
 
-toJSON()
+parseCSV()
+
+
+
+//get all projects
+// function getAllProjectData(){
+//   const projects = axios.get("https://www.toggl.com/api/v8/workspaces/4051800/projects", {
+//     headers: {
+//       'Authorization': 'Basic 92b7f642cb009d898a8d13b0156b794e:api_token'
+//     }
+   
+//   }).then(data => {
+//     console.log(data)
+//   }).catch(error => {
+//     console.log(error)
+//   })
+//   console.log("Success");
+// }
+
+
